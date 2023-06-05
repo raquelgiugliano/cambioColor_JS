ecefaf0f626eb7ad32243ea8b5c1e933d8caab19
raquelgiugliano <raquel.giugliano@gmail.com>
2023-06-05 14:32:33 +0200

    Cambio Color

---
+++ script.js
@@ -0,0 +1,11 @@
+function ToggleMenu() {
+  const menuToggle = document.querySelector(".menuToggle");
+  menuToggle.classList.toggle("active");
+
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("active");
+}
+
+function ImgSlider(parametros) {
+  document.getElementById("slider").src = parametros;
+}
